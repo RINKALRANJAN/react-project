a65a541fafc3bbe18cde8e91b1f7c6486c837648
RINKALRAJAN <rinkalranjan2468@gmail.com>
2023-06-07 06:42:37 +0530

    learning videos

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import MyHeading, { MyHeading1 } from "./components/MyHeading";
+// const a = 200;
+import {MyHeading3} from "./components/MyHeading"
+function App(){
+return(
+    <>
+<div>
+    <MyHeading name ="abhi" text ="nice dude"/>
+    <MyHeading1/>
+    <MyHeading3/>
+    <h1>
+        welcome page 1
+    </h1>
+    <p>hi there</p>
+</div>
+<div>asadas</div>
+</>
+);
+}
+export default App;
---
+++ src/components/MyHeading.jsx
@@ -0,0 +1,34 @@
+// const MyHeading = ()=><h1>heading</h1>;
+// export default MyHeading;
+import React, { Fragment } from 'react'
+
+const MyHeading = (props) => {
+   const obj={
+    abb:12,
+    ccd:2323,
+   };
+const {abb,ccd}=obj;
+
+  return (
+    <Fragment>
+        <h2>{props.name}</h2>
+        <h1>{ccd}</h1>
+    <div>MyHeading</div>
+    </Fragment>
+  )
+}
+const MyHeading1 = () => {
+   
+  return (
+    <p>inside h1</p>
+  )
+}
+const MyHeading3 = () => {
+ 
+  return (
+    <p>inside h2</p>
+  )
+}
+
+export default MyHeading;
+export{MyHeading3,MyHeading1}
