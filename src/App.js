923c7e588a78193a001d395871ecc4e0f613caaf
RINKALRAJAN <rinkalranjan2468@gmail.com>
2023-06-07 09:44:05 +0530

    basic completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 // import MyHeading, { MyHeading1 } from "./components/MyHeading";
 // const a = 200;
 // import {MyHeading3} from "./components/MyHeading"
+
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 import "./styles/App.css";
+import "./styles/Header.css";
+
+
 function App(){
-return(
-<>
-<Header />
-</>
-);
+   return( 
+   <Router>
+    <Header />
+<Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/about" element={<About />}/>
+    <Route path="/contact" element={<Contact />}/>
+</Routes>
+   </Router>
+   );
 }
 export default App;
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import "../styles/Header.css";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return <nav>
-   <a href='/'>home</a>
-   <a href='/'>home</a>
-   <a href='/'>home</a>
+   <Link to ='/'>home</Link>
+   <Link to ='/about'>about</Link>
+   <Link to ='/contact'>contact</Link>
   </nav>;
 };
 
